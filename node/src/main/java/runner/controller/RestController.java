afc941c8d19aac1ca712079081c7ac3e0acbd198
Manul-obmanul <166332685+Manul-obmanul@users.noreply.github.com>
2024-05-22 21:39:44 +0300

    Second

--- node/src/main/java/runner/controller/RestController.java
+++ node/src/main/java/runner/controller/RestController.java
@@ -25,7 +25,7 @@ public class RestController {
         String response = "Шутка сохранена";
         return ResponseEntity.ok(response);
     }
-
+//
     @DeleteMapping("/delete")
     public ResponseEntity<String> deleteJoke(@RequestParam Long id) {
         String response = mainServiceImpl.deleteJoke(id);
